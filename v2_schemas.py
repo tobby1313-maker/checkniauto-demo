af97dc15f3a42edfee6421a60b0fbcbbddb993c0
tobby1313-maker <tobby1313@gmail.com>
2026-09-03 14:24:13 +0200

    Export overview and detail photo schemas

--- v2_schemas.py
+++ v2_schemas.py
@@ -1,5 +1,10 @@
-from v2_schema_photo import PHOTO_SCHEMA
+from v2_schema_photo import PHOTO_OVERVIEW_SCHEMA, PHOTO_SCHEMA
 from v2_schema_research import RESEARCH_SCHEMA
 from v2_schema_report import FINAL_SCHEMA
 
-__all__ = ["PHOTO_SCHEMA", "RESEARCH_SCHEMA", "FINAL_SCHEMA"]
+__all__ = [
+    "PHOTO_OVERVIEW_SCHEMA",
+    "PHOTO_SCHEMA",
+    "RESEARCH_SCHEMA",
+    "FINAL_SCHEMA",
+]
