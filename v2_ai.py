0ab1d25b1a6687c26be9792bdd61b9e46ba8fdc2
tobby1313-maker <tobby1313@gmail.com>
2026-09-03 14:25:35 +0200

    Route V2 vision through the two-tier gallery pipeline

--- v2_ai.py
+++ v2_ai.py
@@ -3,12 +3,12 @@ from v2_ai_tasks import (
     _clamp,
     _list_of_dicts,
     _list_of_strings,
-    analyze_photos,
     research_vehicle,
     synthesize_report,
     unavailable_photo,
     unavailable_research,
 )
+from v2_photo_pipeline import analyze_photos
 
 __all__ = [
     "call_generate_content_json",
